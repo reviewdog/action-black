1d9146fa7192ef8810e2a53895b6e9345df0c031
rickstaa <rick.staa@outlook.com>
2020-12-17 11:40:07 +0100

    :bug: Fixes github workspace cd bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
-  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+  cd "${GITHUB_WORKSPACE}" || exit
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
@@ -34,20 +34,19 @@ fi
 # Run black with reviewdog
 if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
   # work only fix diff suggestion
-  cd "${GITHUB_WORKSPACE}" || exit
   black --diff --quiet "${INPUT_WORKDIR}/${input_args}" 2>&1 \
     | reviewdog -f="diff"                                    \
     -f.diff.strip=0                                          \
-    -name="${INPUT_TOOL_NAME}-fix"                                   \
+    -name="${INPUT_TOOL_NAME}-fix"                           \
     -reporter="github-pr-review"                             \
     -filter-mode="diff_context"                              \
-    -level="${INPUT_LEVEL}"                                     \
-    -fail-on-error="${INPUT_FAIL_ON_ERROR}"                     \
+    -level="${INPUT_LEVEL}"                                  \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}"                  \
     ${INPUT_REVIEWDOG_FLAGS}
 else
-  black --check "${input_args}" 2>&1                 \
-    | reviewdog -f="black"                         \
-    -name="${INPUT_TOOL_NAME}"                             \
+  black --check "${INPUT_WORKDIR}/${input_args}" 2>&1 \
+    | reviewdog -f="black"                            \
+    -name="${INPUT_TOOL_NAME}"                        \
     -reporter="${INPUT_REPORTER:-github-pr-check}"    \
     -filter-mode="${INPUT_FILTER_MODE}"               \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}"           \
