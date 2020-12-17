ebdb49927f5f67486a3f48ec2c4fe8ccf7f8622b
rickstaa <rick.staa@outlook.com>
2020-12-17 11:25:40 +0100

    :art: :bug: Fixes globing problem

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,21 +15,21 @@ else
 fi
 
 # Parse reviewdog_flags action argument to filter duplicates
-if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-name"* ]; then
-  rd_name=${INPUT_TOOL_NAME:-black-format}
-fi
-if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-reporter"* ]; then
-  rd_reporter=${INPUT_REPORTER:-github-pr-check}
-fi
-if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-filter-mode"* ]; then
-  rd_filter_mode=${INPUT_FILTER_MODE:-added}
-fi
-if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-fail-on-error"* ]; then
-  rd_fail_on_error=${INPUT_FAIL_ON_ERROR:-false}
-fi
-if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-level"* ]; then
-  rd_level=${INPUT_FAIL_ON_ERROR:-false}
-fi
+# if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-name"* ]; then
+#   rd_name=${INPUT_TOOL_NAME:-black-format}
+# fi
+# if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-reporter"* ]; then
+#   rd_reporter=${INPUT_REPORTER:-github-pr-check}
+# fi
+# if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-filter-mode"* ]; then
+#   rd_filter_mode=${INPUT_FILTER_MODE:-added}
+# fi
+# if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-fail-on-error"* ]; then
+#   rd_fail_on_error=${INPUT_FAIL_ON_ERROR:-false}
+# fi
+# if [ "${INPUT_REVIEWDOG_FLAGS}" != *"-level"* ]; then
+#   rd_level=${INPUT_FAIL_ON_ERROR:-false}
+# fi
 
 # Run black with reviewdog
 if [ "${rd_reporter}" = 'github-pr-review' ]; then
@@ -43,14 +43,14 @@ if [ "${rd_reporter}" = 'github-pr-review' ]; then
     -filter-mode="diff_context"                              \
     -level="${rd_level}"                                     \
     -fail-on-error="${rd_fail_on_error}"                     \
-    "${INPUT_REVIEWDOG_FLAGS}"
+    ${INPUT_REVIEWDOG_FLAGS}
 else
-  black --check ${input_args} 2>&1                 \
+  black --check "${input_args}" 2>&1                 \
     | reviewdog -f="black"                         \
     -name="${rd_name}"                             \
     -reporter="${rd_reporter:-github-pr-check}"    \
     -filter-mode="${rd_filter_mode}"               \
     -fail-on-error="${rd_fail_on_error}"           \
     -level="${rd_level}"                           \
-    "${INPUT_REVIEWDOG_FLAGS}"
+    ${INPUT_REVIEWDOG_FLAGS}
 fi
