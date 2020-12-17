a726587ed124305acaccd23078d49e0cf6d95753
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-12-17 17:37:27 +0900

    style fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # If no arguments are given use current working directory
 if [ $# -eq 0 ]; then
-    input_args="."
+  input_args="."
 else
   input_args="$*"
 fi
@@ -19,12 +19,12 @@ if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
   echo "ERROR: github-pr-review unsupported (for the black formatter)"
   exit 1
 else
-  black --check ${input_args} 2>&1 \
-    | reviewdog -f="black" \
-        -name="${INPUT_TOOL_NAME}" \
-        -reporter="${INPUT_REPORTER:-github-pr-check}" \
-        -filter-mode="${INPUT_FILTER_MODE}" \
-        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-        -level="${INPUT_LEVEL}" \
-        ${INPUT_REVIEWDOG_FLAGS}
+  black --check ${input_args} 2>&1                 \
+    | reviewdog -f="black"                         \
+    -name="${INPUT_TOOL_NAME}"                     \
+    -reporter="${INPUT_REPORTER:-github-pr-check}" \
+    -filter-mode="${INPUT_FILTER_MODE}"            \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}"        \
+    -level="${INPUT_LEVEL}"                        \
+    ${INPUT_REVIEWDOG_FLAGS}
 fi
