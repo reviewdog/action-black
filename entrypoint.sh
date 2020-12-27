2d3fcd14c4eccf82ef584084d0a6d4f02869dd05
rickstaa <rick.staa@outlook.com>
2020-12-27 16:00:47 +0100

    :sparkles: Adds ability to also format the code

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,31 +8,45 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # If no arguments are given use current working directory
-if [ $# -eq 0 ]; then
+if [ "$#" -eq 0 ]; then
   input_args="."
 else
   input_args="$*"
 fi
 
-# Run black with reviewdog
+# Setup black and reviewdog commands (Done based on reporter type)
+black_args="${INPUT_WORKDIR}/${input_args}"
+reviewdog_args="\
+  -level=${INPUT_LEVEL:-error} \
+  -fail-on-error=${INPUT_FAIL_ON_ERROR:-false} \
+  ${INPUT_REVIEWDOG_FLAGS}"
 if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
-  # work only fix diff suggestion
-  black --diff --quiet "${INPUT_WORKDIR}/${input_args}" 2>&1 \
-    | reviewdog -f="diff"                                    \
-    -f.diff.strip=0                                          \
-    -name="${INPUT_TOOL_NAME}-fix"                           \
-    -reporter="github-pr-review"                             \
-    -filter-mode="diff_context"                              \
-    -level="${INPUT_LEVEL}"                                  \
-    -fail-on-error="${INPUT_FAIL_ON_ERROR}"                  \
-    ${INPUT_REVIEWDOG_FLAGS}
+  black_args="${black_args} --diff --quiet"
+  reviewdog_args="
+    ${reviewdog_args} \
+    -f=diff \
+    -f.diff.strip=0 \
+    -name=${INPUT_TOOL_NAME:-black}-fix \
+    -report=github-pr-review \
+    -filter-mode=diff_context"
 else
-  black --check "${INPUT_WORKDIR}/${input_args}" 2>&1 \
-    | reviewdog -f="black"                            \
-    -name="${INPUT_TOOL_NAME}"                        \
-    -reporter="${INPUT_REPORTER:-github-pr-check}"    \
-    -filter-mode="${INPUT_FILTER_MODE}"               \
-    -fail-on-error="${INPUT_FAIL_ON_ERROR}"           \
-    -level="${INPUT_LEVEL}"                           \
-    ${INPUT_REVIEWDOG_FLAGS}
+  black_args="${black_args} --check"
+  reviewdog_args=" \
+    -f=black \
+    -name=${INPUT_TOOL_NAME:-black} \
+    -reporter=${INPUT_REPORTER:-github-pr-check} \
+    -filter-mode=${INPUT_FILTER_MODE:-added}"
+fi
+
+# Run black (with reviewdog)
+if [ "${INPUT_ANNOTATE:-true}" = 'true' ]; then
+  black ${black_args} --quiet 2>&1 | reviewdog ${reviewdog_args}
+else
+  black ${black_args} --quiet
+fi
+
+# Also format code if this is requested
+# NOTE: Usefull for writing back changes or creating a pull request.
+if [ "${INPUT_FROMAT:-false}" = 'true' ]; then
+  black "${INPUT_WORKDIR}/${input_args}"
 fi
