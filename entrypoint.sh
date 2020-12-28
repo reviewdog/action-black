b8677fc8a288fa0f92c41e3f42e6f4d9e19f1ab4
rickstaa <rick.staa@outlook.com>
2020-12-28 11:23:04 +0100

    :bug: Fixes syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,33 +7,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-# # If no arguments are given use current working directory
-# if [[ "$#" -eq 0 ]]; then
-#   if [[ "${INPUT_WORKDIR}" = "." || "${INPUT_WORKDIR}" = "" ]]; then
-#     black_args="."
-#   else
-#     black_args="${INPUT_WORKDIR}"
-#   fi
-# else
-#   # Check if cmd line input argscontain non option arguments
-#   contains_path="false"
-#   for input_arg in "$@"
-#   do
-#     if [[ "${input_arg}" != -* ]]; then
-#       contains_path="true"
-#     fi
-#   done
-
-#   # Create black input argumnet
-#   # NOTE: If workdir is defined it takes precedence over any paths specified in
-#   # the container input args.
-#   if [[ "${INPUT_WORKDIR}" = "." && "${contains_path}" = 'true' ]]; then
-#       black_args="$*"
-#   else
-#     black_args="${INPUT_WORKDIR} $*"
-#   fi
-# fi
-
+# If no arguments are given use current working directory
 if [[ "$#" -eq 0 ]]; then
   black_args="."
 else
@@ -47,7 +21,7 @@ if [[ "${INPUT_ANNOTATE}" = 'true' ]]; then
   if [[ "${INPUT_REPORTER}" = 'github-pr-review' ]]; then
     echo "[action-black] Checking python code with the black formatter2 and reviewdog..."
     # work only fix diff suggestion
-    black --diff --quiet "${INPUT_WORKDIR}/${input_args}" 2>&1 \
+    black --diff --quiet "${INPUT_WORKDIR}/${black_args}" 2>&1 \
       | reviewdog -f="diff"                                    \
       -f.diff.strip=0                                          \
       -name="${INPUT_TOOL_NAME}-fix"                           \
@@ -61,7 +35,7 @@ if [[ "${INPUT_ANNOTATE}" = 'true' ]]; then
       # fi
   else
     echo "[action-black] Checking python code with the black formatter and reviewdog..."
-    black --check "${INPUT_WORKDIR}/${input_args}" 2>&1 \
+    black --check "${INPUT_WORKDIR}/${black_args}" 2>&1 \
       | reviewdog -f="black"                            \
       -name="${INPUT_TOOL_NAME}"                        \
       -reporter="${INPUT_REPORTER}"                     \
@@ -75,14 +49,14 @@ if [[ "${INPUT_ANNOTATE}" = 'true' ]]; then
   fi
 else
   echo "[action-black] Checking python code using the black formatter..."
-  black --check "${INPUT_WORKDIR}/${input_args}" 2>&1 #|| black_error="true"
+  black --check "${INPUT_WORKDIR}/${black_args}" 2>&1 #|| black_error="true"
 fi
 
 # Also format code if this is requested
 # NOTE: Useful for writing back changes or creating a pull request.
 if [[ "${INPUT_FORMAT}" = 'true'&& "${black_error}" = 'true' ]]; then
   echo "[action-black] Formatting python code using the black formatter..."
-  black "${INPUT_WORKDIR}/${input_args}" || black_error="true"
+  black "${INPUT_WORKDIR}/${black_args}" || black_error="true"
 elif [[ "${INPUT_FORMAT}" = 'true' && "${black_error}" != 'true' ]]; then
   echo "[action-black] Formatting not needed."
 fi
