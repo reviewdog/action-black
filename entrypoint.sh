c9f2a40d1fa54deaa36daa684063d4b81162689a
rickstaa <rick.staa@outlook.com>
2020-12-28 12:25:11 +0100

    :art: Fixes syntax bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,40 +21,40 @@ if [[ "${INPUT_ANNOTATE}" = 'true' ]]; then
   if [[ "${INPUT_REPORTER}" = 'github-pr-review' ]]; then
     echo "[action-black] Checking python code with the black formatter and reviewdog..."
     # work only fix diff suggestion
-    black --diff --quiet "${INPUT_WORKDIR}/${black_args}" 2>&1 \
-      | reviewdog -f="diff"                                    \
-      -f.diff.strip=0                                          \
-      -name="${INPUT_TOOL_NAME}-fix"                           \
-      -reporter="github-pr-review"                             \
-      -filter-mode="diff_context"                              \
-      -level="${INPUT_LEVEL}"                                  \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}"                  \
-      ${INPUT_REVIEWDOG_FLAGS} || reviewdog_error="true"
-      if [[ "${PIPESTATUS[0]}" ]]; then
-        black_error="true"
-      fi
+    black --diff --quiet "${INPUT_WORKDIR}/${black_args}" 2>&1 |
+      reviewdog -f="diff" \
+        -f.diff.strip=0 \
+        -name="${INPUT_TOOL_NAME}-fix" \
+        -reporter="github-pr-review" \
+        -filter-mode="diff_context" \
+        -level="${INPUT_LEVEL}" \
+        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+        ${INPUT_REVIEWDOG_FLAGS} || reviewdog_error="true"
+    if [[ "${PIPESTATUS[0]}" ]]; then
+      black_error="true"
+    fi
   else
     echo "[action-black] Checking python code with the black formatter and reviewdog..."
-    black --check "${INPUT_WORKDIR}/${black_args}" 2>&1 \
-      | reviewdog -f="black"                            \
-      -name="${INPUT_TOOL_NAME}"                        \
-      -reporter="${INPUT_REPORTER}"                     \
-      -filter-mode="${INPUT_FILTER_MODE}"               \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}"           \
-      -level="${INPUT_LEVEL}"                           \
-      ${INPUT_REVIEWDOG_FLAGS} || reviewdog_error="true"
-      if [[ "${PIPESTATUS[0]}" ]]; then
-        black_error="true"
-      fi
+    black --check "${INPUT_WORKDIR}/${black_args}" 2>&1 |
+      reviewdog -f="black" \
+        -name="${INPUT_TOOL_NAME}" \
+        -reporter="${INPUT_REPORTER}" \
+        -filter-mode="${INPUT_FILTER_MODE}" \
+        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+        -level="${INPUT_LEVEL}" \
+        ${INPUT_REVIEWDOG_FLAGS} || reviewdog_error="true"
+    if [[ "${PIPESTATUS[0]}" ]]; then
+      black_error="true"
+    fi
   fi
 else
   echo "[action-black] Checking python code using the black formatter..."
-  black --check "${INPUT_WORKDIR}/${black_args}" 2>&1 #|| black_error="true"
+  black --check "${INPUT_WORKDIR}/${black_args}" 2>&1 || black_error="true"
 fi
 
 # Also format code if this is requested
 # NOTE: Useful for writing back changes or creating a pull request.
-if [[ "${INPUT_FORMAT}" = 'true'&& "${black_error}" = 'true' ]]; then
+if [[ "${INPUT_FORMAT}" = 'true' && "${black_error}" = 'true' ]]; then
   echo "[action-black] Formatting python code using the black formatter..."
   black "${INPUT_WORKDIR}/${black_args}" || black_error="true"
 elif [[ "${INPUT_FORMAT}" = 'true' && "${black_error}" != 'true' ]]; then
@@ -62,6 +62,6 @@ elif [[ "${INPUT_FORMAT}" = 'true' && "${black_error}" != 'true' ]]; then
 fi
 
 # Throw error if an error occurred and fail_on_error is true
-if [[ ( "${reviewdog_error}" = 'true'  || "${black_error}" ) &&  "${INPUT_FAIL_ON_ERROR}" = 'true' ]]; then
+if [[ ("${reviewdog_error}" = 'true' || "${black_error}") && "${INPUT_FAIL_ON_ERROR}" = 'true' ]]; then
   exit 1
 fi
