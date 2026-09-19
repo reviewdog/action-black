44c1cd3954acc1d6b5c9f8bf7ef9be568f2b18f1
shogo82148 <1157344+shogo82148@users.noreply.github.com>
2026-09-19 17:16:55 +0000

    chore(deps): update reviewdog to 0.21.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-export REVIEWDOG_VERSION=v0.21.1
+export REVIEWDOG_VERSION=v0.21.2
 
 echo "[action-black] Installing reviewdog..."
 wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/fd59714416d6d9a1c0692d872e38e7f8448df4fc/install.sh | sh -s -- -b /tmp "${REVIEWDOG_VERSION}"
